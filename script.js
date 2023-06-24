67c2dc83007b721d3545278fcf5969eea5ecd281
t.a.mirfin@gmail.com <t.a.mirfin@gmail.com>
2023-06-24 12:12:44 +0100

    added timer and updated so questions show on the webpage

--- script.js
+++ script.js
@@ -1,51 +1,92 @@
+
+const questionhtml = document.querySelector('.questionhtml');
+const answer1 = document.querySelector('.answer1');
+const answer2 = document.querySelector('.answer2');
+const answer3 = document.querySelector('.answer3');
+const answer4 = document.querySelector('.answer4');
+const timeLeft = document.querySelector('.timer');
+
 const question = ["What is the capital city of Australia?", 'Who painted the Mona Lisa?', 'Which country is known for producing the most coffee in the world?'];
 const answers = ['Canberra', 'Leonardo da Vinci', 'Brazil'];
 const wrongAnswers = [['Sydney', 'Melbourne', 'Brisbane'], ['Pablo Picasso', 'Vincent van Gogh', 'Michelangelo'], ['Colombia', 'Ethiopia', 'Vietnam']];
 
 let gameOver = false;
-let guesses = 0;
 let currentQuestionIndex = 0;
 let correct = 0;
-let maxGuesses = 3;
+const maxGuesses = 3;
+const timerDuration = 10;
+let timer;
 
-while (!gameOver) {
-    const currentQuestion = question[currentQuestionIndex];
-    console.log(currentQuestion);
+function displayQuestionAndAnswers() {
+    questionhtml.textContent = question[currentQuestionIndex];
 
     const allAnswers = [answers[currentQuestionIndex], ...wrongAnswers[currentQuestionIndex]];
-    console.log('Available answers:');
-    console.log(allAnswers);
+    answer1.textContent = allAnswers[0];
+    answer2.textContent = allAnswers[1];
+    answer3.textContent = allAnswers[2];
+    answer4.textContent = allAnswers[3];
 
 
+    clearTimeout(timer);
+    startTimer();
+}
 
-    let userAnswer = prompt('What is your answer?');
+function startTimer() {
+    timeRemaining = timerDuration;
+    updateTimer();
 
-    if (userAnswer === null) {
-        console.log('Game exited.');
-        gameOver = true;
+    timer = setInterval(() => {
+        timeRemaining--;
+        updateTimer();
 
-    } else if (userAnswer === answers[currentQuestionIndex]) {
-        console.log('Correct!');
-        correct++
-        currentQuestionIndex++; // Move to the next question
-        if (currentQuestionIndex === question.length) {
-            console.log(`well done! you got ${correct}, out of 3`);
-            gameOver = true;
+        if (timeRemaining === 0) {
+            clearInterval(timer);
+            endGame();
         }
+    }, 1000);
+}
+
+function updateTimer() {
+    // Update the timer element with the remaining time
+    const timerElement = document.querySelector('.timer');
+    timerElement.textContent = `Time Remaining: ${timeRemaining} seconds`;
+}
+
+
+
+displayQuestionAndAnswers();
+
+function checkAnswer(selectedAnswer) {
+    if (selectedAnswer === answers[currentQuestionIndex]) {
+        questionhtml.textContent = 'Correct!';
+        correct++;
     } else {
-        console.log('Oops that was wrong');
+        questionhtml.textContent = 'Oops, that was wrong';
+    }
 
-        currentQuestionIndex++;
-        if (currentQuestionIndex === question.length) {
-            console.log(`Oh Dear, you got ${correct}, out of 3`);
-            gameOver = true;
-        }
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === question.length || currentQuestionIndex === maxGuesses || timeRemaining === 0) {
+        gameOver = true;
+        questionhtml.innerHTML = `Game over! You got ${correct} correct out of ${question.length}`;
+    } else {
+        setTimeout(displayQuestionAndAnswers, 1000);
     }
 }
 
-//      (guesses === maxGuesses) {
-//         console.log('GAME OVER, you are our of lives');
-//         guesses++;
-//         gameOver = true;
-//     }
-// ###
+answer1.addEventListener('click', function () {
+    checkAnswer(answer1.textContent);
+});
+
+answer2.addEventListener('click', function () {
+    checkAnswer(answer2.textContent);
+});
+
+answer3.addEventListener('click', function () {
+    checkAnswer(answer3.textContent);
+});
+
+answer4.addEventListener('click', function () {
+    checkAnswer(answer4.textContent);
+});
+
